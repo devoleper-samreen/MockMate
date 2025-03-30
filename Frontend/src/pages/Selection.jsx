2604a04ce0447ab33e03e4ffca41c7a4b425be33
devoleper-samreen <samreenmalik52292@gmail.com>
2025-03-30 16:25:02 +0530

    Update SelectionCard component: add status prop to indicate availability of interview options

--- Frontend/src/pages/Selection.jsx
+++ Frontend/src/pages/Selection.jsx
@@ -88,9 +88,9 @@ const InterviewSelectionPage = () => {
         <div className="min-h-[90vh] bg-gray-900 text-white flex flex-col items-center px-10">
             <h1 className="text-5xl font-bold text-yellow-400 my-12">Start Your Interview</h1>
             <div className="mt-12 grid grid-cols-1 md:grid-cols-3 gap-8 max-w-5xl w-full">
-                <SelectionCard icon={<FaUserTie />} title="Become an Interviewer" description="Host an interview and help others prepare." onClick={() => navigate("/become-interviewer")} />
-                <SelectionCard icon={<FaCalendarAlt />} title="Schedule an Interview" description="Plan your interview with an interviewer." onClick={() => navigate("/schedule-interview")} />
-                <SelectionCard icon={<FaRocket />} title="Instant Interview" description="Start an interview now. We can connect you with a peer or interviewer." onClick={handleInstantInterview} />
+                <SelectionCard icon={<FaUserTie />} status='Coming Soon...' title="Become an Interviewer" description="Host an interview and help others prepare." onClick={() => navigate("/become-interviewer")} />
+                <SelectionCard icon={<FaCalendarAlt />} status='Coming Soon...' title="Schedule an Interview" description="Plan your interview with an interviewer." onClick={() => navigate("/schedule-interview")} />
+                <SelectionCard icon={<FaRocket />} status='Live' title="Instant Interview" description="Start an interview now. We can connect you with a peer or interviewer." onClick={handleInstantInterview} />
             </div>
 
             <Modal open={isMatching} footer={null} onCancel={() => setIsMatching(false)} centered>
@@ -112,9 +112,12 @@ const InterviewSelectionPage = () => {
     );
 };
 
-const SelectionCard = ({ icon, title, description, onClick }) => (
+const SelectionCard = ({ icon, title, description, onClick, status }) => (
     <div className="bg-gray-800 p-8 rounded-lg shadow-lg text-center cursor-pointer hover:scale-105 transition-transform" onClick={onClick}>
-        <div className="text-4xl mb-4 text-yellow-400">{icon}</div>
+        <div className="flex text-4xl mb-6 text-yellow-400 items-center justify-between">
+            {icon}
+            <span className="text-sm text-gray-400 font-bold">{status}</span>
+        </div>
         <h3 className="text-2xl font-bold mb-2">{title}</h3>
         <p className="text-gray-300">{description}</p>
     </div>
